a2f1ddbfe36aa8f5b42142306f874ce44609ee81
Brendan Coletta <bc.coletta@gmail.com>
2020-01-31 16:35:40 -0600

    History routing and fixed contact buttons

--- src/components/home/Masthead.vue
+++ src/components/home/Masthead.vue
@@ -12,7 +12,7 @@
             </p>
           </div>
           <div class="six wide right floated column">
-            <div class="ui left floated attached vertical buttons">
+            <div id="computer-buttons" class="ui left floated attached vertical buttons">
               <a
                 class="ui linkedin icon button"
                 href="https://www.linkedin.com/in/brendancoletta/"
@@ -33,11 +33,51 @@
               </a>
             </div>
             <img
+              id="profile-image"
               src="@/assets/LinkedInProfilePicture1.jpg"
               class="ui left floated image"
             />
+            <div id="tablet-buttons" class="ui bottom attached buttons">
+              <a
+                class="ui linkedin icon button"
+                href="https://www.linkedin.com/in/brendancoletta/"
+              >
+                <i class="linkedin icon"></i>
+              </a>
+              <a
+                class="ui blue icon button"
+                href="https://drive.google.com/file/d/12flN6d7kdUZBOVjfZ-_CLDxfEvfFbE7p/view"
+              >
+                <i class="file alternate outline icon"></i>
+              </a>
+              <a
+                href="mailto:bc.coletta+website@gmail.com"
+                class="ui google plus icon button"
+              >
+                <i class="mail icon"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
 </template>
+
+<style lang="less" scoped>
+@media only screen and (max-width: 992px) {
+  #computer-buttons {
+    display: none;
+  }
+  #profile-image {
+    margin-bottom: 0;
+  }
+  #tablet-buttons {
+    float: left;
+  }
+}
+@media only screen and (min-width: 993px) {
+  #tablet-buttons {
+    display: none;
+  }
+}
+</style>
